7db8cab63cab62a74404f281980854d6bcfef8d0
Patricia82TI <patricia.melloti@gmail.com>
2024-11-25 16:53:13 -0300

    Programa usando for loop

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Contando de 1 a 10
+        for(int i = 1; i <= 10; i++)
+            System.out.print(i + " ");
+        }
+    }
